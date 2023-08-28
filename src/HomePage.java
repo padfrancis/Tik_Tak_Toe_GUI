55ce5f1429e6582f85818d8d2617c2df21d7f59e
padfrancis <francispadron@spcis.edu.ph>
2023-08-28 20:44:17 +0800

    Added textbox and designed the button

--- src/HomePage.java
+++ src/HomePage.java
@@ -16,7 +16,7 @@ public class HomePage extends JFrame implements ActionListener, KeyListener {
     Clip clip = AudioSystem.getClip();
     JTextField text = new JTextField(10);
     ImageIcon home = new ImageIcon("src/icons/home.gif");
-    JLabel title = new JLabel("Tic Tac Toe!");
+    JLabel title = new JLabel("Welcome To Tic Tac Toe!");
     JButton Log = new JButton("Play");
     JLabel Bg = new JLabel();
     HomePage() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
@@ -25,6 +25,7 @@ public class HomePage extends JFrame implements ActionListener, KeyListener {
         this.setSize(500,500);
         Bg.setBounds(0, 0, getWidth(), getHeight());
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setTitle("Tik Tak Toe");
         this.setLayout(null);
         this.add(Log);
         this.add(text);
@@ -35,13 +36,19 @@ public class HomePage extends JFrame implements ActionListener, KeyListener {
     void Setup(){
         Bg.setIcon(home);
 
-        Log.setBounds(0,0, 100,100);
-        Log.setOpaque(false);
+        Log.setBounds(185,300, 100,30);
+        //Log.setOpaque(false);
         Log.addActionListener(this);
         Log.addKeyListener(this);
         Log.setFocusable(false);
         Log.setBorderPainted(false);
-        Log.setBackground(new Color(0,0,0,0));
+        Log.setBackground(new Color(134,69,23));
+
+        text.setFont(new Font("Arial", Font.PLAIN, 20));
+        text.setBounds(80,200, 300,50);
+        text.setHorizontalAlignment(SwingConstants.CENTER);
+        text.setBackground(Color.lightGray);
+        text.setForeground(Color.BLACK);
     }
     void Loop_Music() throws LineUnavailableException, IOException {
         clip.open(m);
