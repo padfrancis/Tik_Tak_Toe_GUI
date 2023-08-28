591bcae17f19332ff053cdb8634551cb0ff7ae1e
padfrancis <francispadron@spcis.edu.ph>
2023-08-28 14:09:30 +0800

    Added 2 new files for the game

---
+++ src/Game_Frame.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Game_Frame extends JFrame {
+    Game_Frame() {
+        this.setSize(600,840);
+        this.setVisible(true);
+    }
+}
---
+++ src/HomePage.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class HomePage extends JFrame {
+    HomePage() {
+        this.setSize(500,500);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    }
+}
