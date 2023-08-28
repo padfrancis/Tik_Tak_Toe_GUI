41a9399793e089f5d803832f868a518a74966421
padfrancis <francispadron@spcis.edu.ph>
2023-08-28 15:31:29 +0800

    Designed and added BG Music on HomePage

--- src/HomePage.java
+++ src/HomePage.java
@@ -1,9 +1,91 @@
+import javax.sound.sampled.spi.AudioFileReader;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
 
-public class HomePage extends JFrame {
-    HomePage() {
+public class HomePage extends JFrame implements ActionListener, KeyListener {
+    String lmusic = "src/audios/lobby-music.wav";
+    File music = new File(lmusic);
+    AudioInputStream m = AudioSystem.getAudioInputStream(music);
+    Clip clip = AudioSystem.getClip();
+    JTextField text = new JTextField(10);
+    ImageIcon home = new ImageIcon("src/icons/home.gif");
+    JLabel title = new JLabel("Tic Tac Toe!");
+    JButton Log = new JButton("Play");
+    JLabel Bg = new JLabel();
+    HomePage() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        Loop_Music();
+        Setup();
         this.setSize(500,500);
+        Bg.setBounds(0, 0, getWidth(), getHeight());
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.add(Log);
+        this.add(text);
+        this.add(Bg);
+        this.setIconImage(null);
+        this.setVisible(true);
+    }
+    void Setup(){
+        Bg.setIcon(home);
+
+        Log.setBounds(0,0, 100,100);
+        Log.setOpaque(false);
+        Log.addActionListener(this);
+        Log.addKeyListener(this);
+        Log.setFocusable(false);
+        Log.setBorderPainted(false);
+        Log.setBackground(new Color(0,0,0,0));
+    }
+    void Loop_Music() throws LineUnavailableException, IOException {
+        clip.open(m);
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==Log) {
+            if(text.getText().equals("")){
+                Log.setEnabled(false);
+                JOptionPane.showMessageDialog(null, "Please enter a name to proceed.", "ERROR!", JOptionPane.WARNING_MESSAGE);
+            }else{
+                clip.stop();
+                clip.close();
+                try {
+                    m.close();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+            Log.setEnabled(true);
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        if (keyCode == KeyEvent.VK_ENTER) {
+            Log.doClick();
+        }
+    }
+    @Override
+    public void keyReleased(KeyEvent e) {
 
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.*;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        new Game_Frame();
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    new HomePage();
+                } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
     }
 }
