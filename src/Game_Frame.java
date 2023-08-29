cd24590816209d37c510dce45373dcc9013335f2
padfrancis <francispadron@spcis.edu.ph>
2023-08-29 09:13:46 +0800

    Updated GameFrame

--- src/Game_Frame.java
+++ src/Game_Frame.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 public class Game_Frame extends JFrame {
     Game_Frame() {
         this.setSize(600,840);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setVisible(true);
     }
 }
